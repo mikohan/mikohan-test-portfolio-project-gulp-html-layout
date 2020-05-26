5625db8adf1a228198fddda3bbfa63306aa3faa9
Vladimir <angara99@gmail.com>
2020-05-26 22:09:38 +0300

    Intital commit and finished home page

---
+++ src/assets/js/vendor/jqueryBootstrap.js
@@ -0,0 +1,9 @@
+//= ../../../../node_modules/jquery/dist/jquery.min.js
+
+// Импортируем Popper
+//= ../../../../node_modules/@popperjs/core/dist/umd/popper.js
+
+// Импортируем необходимые js-файлы Bootstrap 4
+//= ../../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../../node_modules/bootstrap/js/dist/modal.js
+
